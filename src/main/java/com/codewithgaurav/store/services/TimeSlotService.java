9a726644c8a698acc81fbb74c3783491d6a22013
Gaurav-kushwaha20 <gaurav.kushwaha.0825@gmail.com>
2025-06-10 20:59:30 +0545

    create time slot

---
+++ src/main/java/com/codewithgaurav/store/services/TimeSlotService.java
@@ -0,0 +1,38 @@
+package com.codewithgaurav.store.services;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.codewithgaurav.store.model.TimeSlotModel;
+import com.codewithgaurav.store.repository.futsal.TimeSlotRepository;
+
+@Service
+public class TimeSlotService {
+
+   @Autowired
+   private TimeSlotRepository timeSlotRepo;
+
+   // Save the time slot
+   public TimeSlotModel createTimeSlot(TimeSlotModel timeSlot) {
+      try {
+         return timeSlotRepo.save(timeSlot);
+      } catch (Exception e) {
+         throw new RuntimeException("Failed to save time slot: " + e.getMessage());
+      }
+   }
+
+   // get the time slot
+   public List<TimeSlotModel> getSlotsByFutsal(String futsalId) {
+      return timeSlotRepo.findByFutsalId(futsalId);
+   }
+
+   // Is Futsal TimeSlot exist
+   public boolean isTimeSlotExist(String futsalId, LocalDate date, LocalTime startTime) {
+      return timeSlotRepo.existsByFutsalIdAndDateAndStartTime(futsalId, date, startTime);
+   }
+
+}
