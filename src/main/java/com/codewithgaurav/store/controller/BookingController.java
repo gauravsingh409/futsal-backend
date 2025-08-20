32223c55eab3ca6d3ad3cf4756b93a0aa0e00c66
Gaurav-kushwaha20 <gaurav.kushwaha.0825@gmail.com>
2025-08-20 22:11:44 +0545

    feat: booking api

--- src/main/java/com/codewithgaurav/store/controller/BookingController.java
+++ src/main/java/com/codewithgaurav/store/controller/BookingController.java
@@ -1,6 +1,11 @@
 package com.codewithgaurav.store.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.codewithgaurav.store.dto.response.BookingResponseDto;
 import com.codewithgaurav.store.entity.BookingEntity;
 import com.codewithgaurav.store.payload.ApiResponse;
+import com.codewithgaurav.store.payload.PaginatedResponse;
 import com.codewithgaurav.store.services.BookingService;
 import com.codewithgaurav.store.services.JwtService;
 
@@ -43,6 +49,48 @@ public class BookingController {
         BookingEntity bookingEntity = bookingService.saveBooking(request, futsalId, userId);
         BookingResponseDto bookingResponseDto = bookingService.converToDto(bookingEntity);
         return ResponseEntity.ok().body(new ApiResponse<>("Booking success", 200, true, bookingResponseDto));
+    }
+
+    @GetMapping(path = { "/booking/user" })
+    public ResponseEntity<?> getUserBooking(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int pageSize,
+            @RequestParam(required = false) String search,
+            HttpServletRequest httpServletRequest) {
+        jwtService.isValidUser(httpServletRequest);
+        Long id = jwtService.extractId(httpServletRequest);
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<BookingEntity> bookingPage = bookingService.getUserBooking(id, pageable);
+        Page<BookingResponseDto> bookingPageDto = bookingService.convertPageToDto(bookingPage);
+        var response = new PaginatedResponse<>(
+                bookingPageDto.getContent(),
+                bookingPageDto.getNumber(),
+                bookingPageDto.getSize(),
+                bookingPageDto.getTotalElements(),
+                bookingPageDto.getTotalPages());
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>("Booking retrieve successfully", 200, true,
+                response));
+    }
+
+    @GetMapping(path = { "/booking/owner" })
+    public ResponseEntity<?> getOwnerBooking(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int pageSize,
+            @RequestParam(required = false) String search,
+            HttpServletRequest httpServletRequest) {
+        jwtService.isValidAdminOrOwner(httpServletRequest);
+        Long id = jwtService.extractId(httpServletRequest);
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<BookingEntity> bookingPage = bookingService.getUserBooking(id, pageable);
+        Page<BookingResponseDto> bookingPageDto = bookingService.convertPageToDto(bookingPage);
+        var response = new PaginatedResponse<>(
+                bookingPageDto.getContent(),
+                bookingPageDto.getNumber(),
+                bookingPageDto.getSize(),
+                bookingPageDto.getTotalElements(),
+                bookingPageDto.getTotalPages());
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>("Booking retrieve successfully", 200, true,
+                response));
     }
 
 }
