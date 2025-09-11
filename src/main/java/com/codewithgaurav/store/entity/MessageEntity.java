53e8e2ba256beb0e408bd5c3250762d371572ee0
Gaurav-kushwaha20 <gaurav.kushwaha.0825@gmail.com>
2025-09-11 21:19:37 +0545

    message and conversation entity

---
+++ src/main/java/com/codewithgaurav/store/entity/MessageEntity.java
@@ -0,0 +1,49 @@
+package com.codewithgaurav.store.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "messages")
+public class MessageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "conversation_id")
+    private ConversationEntity conversation;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "sender_id")
+    private UserEntity sender;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.SENT;
+
+    
+    
+    
+    
+    public enum Status {
+        SENT, DELIVERED, READ
+    }
+
+}
